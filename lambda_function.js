fe0d7c77fbde0dd2a51f5dd6481e4e9c7f735139
Chris Feldmann <cfeldmann@gmail.com>
2017-07-28 13:28:19 -0400

    working xml2json, database insert

--- lambda_function.js
+++ lambda_function.js
@@ -2,10 +2,11 @@
 
 // require('async');
 const subprocess = require( 'child_process' );
-
+const xml2js = require('xml2js');//.parseString;
 const aws = require('aws-sdk');
 const s3 = new aws.S3({ apiVersion: '2006-03-01' });
 const dynamodb = new aws.DynamoDB.DocumentClient();
+
 
 exports.handler = (event, context, callback) => {
     // console.log('Received event:', JSON.stringify(event, null, 2));
@@ -37,33 +38,44 @@ exports.handler = (event, context, callback) => {
 
             const item = { keyName : params.Key, technicalMetadata : data.ContentType };
             console.log( 'item', item );
-
+            const help = subprocess.spawn('./mediainfo', ['--help']);
+            console.log('help:', help);
             
-            const results = subprocess.spawn('./mediainfo', ['--full', '--output=JSON', data] );
+            const results = subprocess.spawn('./mediainfo', ['--full', '--output=XML', data] );
             // console.log('results', results);
             results.stdout.on('data', (results) => {
-                console.log( 'out args: ', arguments, '\nthose were the args');
+                // console.log( 'out args: ', arguments, '\nthose were the args');
                 console.log('results:', typeof results);
-                //console.log(results);
+                console.log(results);
                 const unbuffed = results.toString();
                 console.log('unbuffed:', unbuffed);
                 
-                // const str = Buffer.from( results );
-                // console.log('results:', str.toSring());
-                console.log(JSON.stringify( unbuffed));
-                console.log(typeof JSON.stringify( unbuffed));
-                dynamodb.put({ TableName : 'TechnicalMetadata', Item : unbuffed }, function( err, dbresults){
-                    console.log( 'trying dynamodb' );
-                    if( err ){
-                        console.log( err );
-                    } else {
-                        console.log( 'succcess', dbresults );
-                    }
-                });    
+                const parser = new xml2js.Parser({ ignoreAttrs : true });
+                parser.parseString( unbuffed, function( err, js_result ){
+                    console.log('inside');
+                    const json_out = JSON.stringify(js_result);
+                    console.log( json_out);
+
+                    const query = { 
+                        TableName : 'TechnicalMetadata', 
+                        Item :  {
+                            "keyName" : item.keyName,
+                            "technicalMetadata" : json_out
+                        }
+                    };
+                    dynamodb.put( query, function( err, dbresults){
+                        console.log( 'trying dynamodb' );
+                        if( err ){
+                            console.log( err );
+                        } else {
+                            console.log( 'succcess', dbresults );
+                        }
+                    });
+                });
+                
+                    
             });
 
-
-            
         }
     });
 };
